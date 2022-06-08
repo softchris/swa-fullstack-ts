be8cc256f08a60596b9e3d20b0eca0d37eb2d8f5
softchris <noringc@gmail.com>
2022-06-08 01:13:01 +0100

    fix api

--- backend/Get/index.ts
+++ backend/Get/index.ts
@@ -1,15 +1,21 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
+class Response {
+    message: string
+}
+
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
+    var response = new Response();
+    response.message = "hello";
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        "content-type": "application/json",
+        body: JSON.stringify(response)
     };
 
 };
